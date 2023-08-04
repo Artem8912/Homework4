3f6b1eefcca5cc7b8d38daac1b6853b4f53d4ccb
Art <artemgoryunov89@gmail.com>
2023-08-04 12:10:03 +0400

    Программа для рассчёта суммы элементов с нечётным индеком

---
+++ Exercise-36/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine(" Введите размер массива ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+Console.WriteLine(" Введите нижний предел массива ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите верхний предел массива ");
+int max = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Random rnd = new Random();
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+int SumofUnevenNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+FillArray(array);
+Console.WriteLine(string.Join(",", array));
+int result = SumofUnevenNum(array);
+Console.WriteLine($"Сумма элементов с нечётным индексом равна {sum}");
